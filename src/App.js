412e3ab1894fbcf3f2019ded41c5cc73a9642b9f
OsmarPSantizo <69328909+OsmarPSantizo@users.noreply.github.com>
2021-06-07 19:50:59 -0600

    prueba paginas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-
+import React from "react";
+import{
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom"
+import ReactDOM from "react-dom";
 import Galeria from "./components/Menu/Galeria";
 import Navbar from "./components/Header/Header";
 
+
 const App = () => {
   return (
-    <>
-    <Navbar />
-    <Galeria />
-        </>
 
+    <Router>
+    <Navbar />
+
+        <hr/>
+        <Switch>
+            <Route path={"/PruebaReactJs"}>
+               <Galeria/>
+            </Route>
+            <Route path={"/contacto"}>
+                Esta es la pagina de contacto
+            </Route>
+            <Route path={"/nosotros"}>
+                Esta es la pagina de nosotros
+            </Route>
+        </Switch>
+        </Router>
 
   );
 }
