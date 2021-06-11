8eb6395af3421984a48d008a8658154851baca6f
OsmarPSantizo <69328909+OsmarPSantizo@users.noreply.github.com>
2021-06-11 14:02:49 -0600

    creación de carpeta

---
+++ Pagina/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Galeria from "./components/Menu/Galeria";
+import Navbar from "./components/Header/Header";
+
+
+const App = () => {
+  return (
+
+    <>
+    <Navbar />
+    <Galeria />
+
+
+        </>
+
+  );
+}
+
+export default App;
